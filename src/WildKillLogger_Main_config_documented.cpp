6ef5e969ddfa7ea9dac1a1d8793a68c3c3c2d830
WildKillLogger Maintainers <maintainers@wildkilllogger.local>
2026-03-24 12:01:24 +0100

    chore: refresh build marker to latest commit timestamp

--- src/WildKillLogger_Main_config_documented.cpp
+++ src/WildKillLogger_Main_config_documented.cpp
@@ -1,7 +1,7 @@
 // Build marker:
 // branch: perf_tuning
-// commit: e227d4481d128ae85e76926978b1396c17588f39
-// commit_timestamp: 2026-03-24T10:05:23+01:00
+// commit: e8dc0d1db6021c6a5cca059430abaf7e07c8b577
+// commit_timestamp: 2026-03-24T11:59:14+01:00
 
 #include "API/ARK/Ark.h"
 
